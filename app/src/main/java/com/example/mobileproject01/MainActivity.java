2709ff7d3e69f90525022b0ae6c780f344ea6c54
arvinAM17 <arvinazarmina@gmail.com>
2019-07-11 18:07:08 +0430

    Added the recyclerView and an adapter file.

--- app/src/main/java/com/example/mobileproject01/MainActivity.java
+++ app/src/main/java/com/example/mobileproject01/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.res.Configuration;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,13 +20,30 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements Observer {
 
-    NotificationCenter notificationCenter=new NotificationCenter();
+    NotificationCenter notificationCenter = new NotificationCenter();
+    private RecyclerView topicsRecyclerView;
+    private RecyclerView.Adapter topicsMAdapter;
+    private RecyclerView.LayoutManager topicsLayoutManager;
+
+    private RecyclerView newsRecyclerView;
+    private RecyclerView.Adapter newsMAdapter;
+    private RecyclerView.LayoutManager newsLayoutManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayList<News> news = new ArrayList<News>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        newsRecyclerView = (RecyclerView) findViewById(R.id.topics);
+        newsRecyclerView.setHasFixedSize(false);
+
+        newsLayoutManager = new LinearLayoutManager(this);
+        newsRecyclerView.setLayoutManager(newsLayoutManager);
+
+        newsMAdapter = new MyAdapter(news);
+        newsRecyclerView.setAdapter(newsMAdapter);
+
 
 //        AppBarConfiguration appBarConfiguration =
     }
